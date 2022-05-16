a765536463901850f6753d24aa93809b7d050a9b
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-05-16 22:11:28 +0200

    completed third section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+    const expenses = [
+        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
+        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
+        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
+        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
+    ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/*{expenses.map((expense) => {*/}
+        {/*    return <ExpenseItem*/}
+        {/*        title={expense.title}*/}
+        {/*        amount={expense.amount}*/}
+        {/*        date={expense.date}*/}
+        {/*    />*/}
+        {/*})}*/}
+        <Expenses expenses={expenses}/>
     </div>
   );
 }
