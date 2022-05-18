8fe46429f55f594df5c32659a10ed19ef33f2bc5
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-05-18 23:15:52 +0200

    completed fifth section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+import {useState} from "react";
+const DUMMY_EXPENSES = [
+    { id: 'el1', title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
+    { id: 'el2', title: 'Medicine', amount: 26, date: new Date(2021, 5, 2)},
+    { id: 'el3', title: 'Entertainment', amount: 76, date: new Date(2021, 9, 813)},
+];
 
 function App() {
-    const expenses = [
-        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
-        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
-        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
-        { title: 'Car Insurance', amount: 267.4, date: new Date(2021, 2, 28)},
-    ]
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+    const addExpenseHandler = (expense) => {
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses]
+        });
+    }
   return (
     <div className="App">
-        {/*{expenses.map((expense) => {*/}
-        {/*    return <ExpenseItem*/}
-        {/*        title={expense.title}*/}
-        {/*        amount={expense.amount}*/}
-        {/*        date={expense.date}*/}
-        {/*    />*/}
-        {/*})}*/}
+        <NewExpense onAddExpense={addExpenseHandler}/>
         <Expenses expenses={expenses}/>
     </div>
   );
